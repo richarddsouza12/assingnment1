3c7311048b1f7108a73e4155a885bfd3a78dcfb8
Richard Dsouza <richarddsouza12@gmail.com>
2024-03-30 15:46:33 +0530

    changes

--- src/test/java/org/example/Tests/TestUrls.java
+++ src/test/java/org/example/Tests/TestUrls.java
@@ -10,33 +10,37 @@ import java.util.List;
 
 public class TestUrls {
 
-    public static int LIMIT = 10;
+    public final int LIMIT = 10;
+    public final String WEBSITE_URL = "https://www.flipkart.com";
 
     @Test
-    public void TestUrlsInFacebookPage() {
+    public void TestUrlsOnFlipkartPage() {
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("headless");
         WebDriver driver = new ChromeDriver( chromeOptions );
-        driver.get("https://www.flipkart.com");
+        driver.get( WEBSITE_URL );
 
+        //print total links found
         List<WebElement> listAWebElement =  driver.findElements( By.tagName("a") );
         System.out.println( "Total <a href> links found : " +  listAWebElement.size() );
-        listAWebElement = listAWebElement.subList( 0 , LIMIT ); /** limit the processing of all links for simplicity */
 
+        // *** limit the processing of all links for simplicity
+        listAWebElement = listAWebElement.subList( 0 , LIMIT );
 
+        //using foreach loop
         System.out.println( "Printing first " + LIMIT + " elements of <a> using foreach loop :" );
         for( WebElement aWebElement : listAWebElement ) {
             System.out.println( aWebElement.getAttribute("href") );
         }
 
-
+        //using streams with lambda expression
         System.out.println( "Printing first " + LIMIT + " elements of <a> using streams with lambda expression" );
         listAWebElement.stream().forEach( ( element  ) -> {
             System.out.println( element.getAttribute("href") );
         } );
 
-
+        //parallel streams with lambda expression
         System.out.println( "Printing first "  + LIMIT + " elements of <a> using parallel streams with lambda expression" );
         listAWebElement.parallelStream().forEach( ( element ) -> {
             System.out.println( element.getAttribute("href") );
